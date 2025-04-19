2b17815cb93c33ea320dc8cf787e7e6b64ef6439
Brayan-Valenzuela <b.rayancamilo97@hotmail.com>
2025-04-18 19:58:42 -0500

    Update documentation

--- README.md
+++
@@ -1,2 +0,0 @@
-# Python-project-template
-Base template for Python based routines.
--- src/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Utils module - Here are defined generic utilities.
-"""
-
-from .logger_control import start_logger, get_logger
-
-__all__ = ["start_logger", "get_logger"]
